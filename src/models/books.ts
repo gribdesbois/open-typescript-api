dfe74c9d71c9cba37548360830e0160c2bb3669d
gribdesbois <gribdesbois@gmail.com>
2022-03-14 11:47:49 +0100

    book model

---
+++ src/models/books.ts
@@ -0,0 +1,28 @@
+import { IBook } from './../types/books'
+import { model, Schema } from 'mongoose'
+import { v4 as uuidv4 } from 'uuid'
+
+const bookSchema: Schema = new Schema(
+  {
+    id: {
+      type: uuidv4,
+      required: true,
+      default: uuidv4(),
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export default model<IBook>('Book', bookSchema)
